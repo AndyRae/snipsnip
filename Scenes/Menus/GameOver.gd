2913b8f7305c59957c68ce6ec93e6da84aa2663d
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-12-19 11:56:27 +0000

    basic menu layout

---
+++ Scenes/Menus/GameOver.gd
@@ -0,0 +1,14 @@
+extends CanvasLayer
+
+const GAME_OVER_MESSAGE = "# GAME OVER"
+const GAME_OVER_SUBTITLE = "You got the snip!"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
