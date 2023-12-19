a26edc3a8abcbd890262add4feb9f9f0951cf024
Shreya Iyer <shreyaiyer@MacBook-Pro-2.local>
2023-12-19 14:25:04 +0000

    Helath control added

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -3,10 +3,17 @@
 extends Node2D
 
 var score : int = 0
+var health : int = 3
 
 # Adds 1 to score variable
 func add_score():
 	score += 1
+	
+func subtract_health():
+	health -= 1
+	
+func add_health():
+	health += 1
 
 # Loads next level
 func load_next_level(next_scene : PackedScene):
--- Scripts/GameUI.gd
+++ Scripts/GameUI.gd
@@ -3,6 +3,10 @@ extends Control
 @onready var score_texture = %Score/ScoreTexture
 @onready var score_label = %Score/ScoreLabel
 
+@onready var health_texture = %Health/HealthTexture
+@onready var health_label = %Health/HealthLabel
+
 func _process(_delta):
 	# Set the score label text to the score variable in game maanger script
 	score_label.text = "x %d" % GameManager.score
+	health_label.text = "x %d" % GameManager.health
